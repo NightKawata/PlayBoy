1fdfe730a28bedb4df65e3cb76df53c79ccbd7ba
NightKawata <kawata@nekuzen.net>
2014-02-11 16:05:16 -0500

    OOPS
    Forgot to get rid of that

--- main.lua
+++ main.lua
@@ -7,7 +7,6 @@
 	~NightKawata
 --]]
 local playBoy = nil -- the PlayBoy instance.
-local deltz = 0
 
 function love.load()
 	playBoy = require("playboy"):new() -- We'll obviously need PlayBoy to do our dirty work.
@@ -15,11 +14,6 @@ function love.load()
 end
 
 function love.update(dt)
-	deltz = deltz + dt
-	if deltz >= 3 then
-		deltz = 0
-		playBoy:sendMessage("hello")
-	end
 	playBoy:update(dt)
 end
 
